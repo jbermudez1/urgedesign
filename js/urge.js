5bfecbdeccb898cdab53c9824ad5e4a45e8c39c7
jbermudez1 <javierbermudez@MacBook-Pro-de-Javier-2.local>
2015-03-10 20:11:58 -0700

    Agregando menu lateral

--- js/urge.js
+++ js/urge.js
@@ -2,8 +2,17 @@ $(document).on( "ready", inicio );
 
 function inicio (){
 	
-	$( "nav" ).load( "menu.html" );
+	$( "nav" ).load( "menu.html", vermenu);
+	
 	$("#filtroDos").hide();
 
 	$("#footer").load("footer.html");
+
+
+}
+
+function vermenu (){
+	$(".verMenu").click( function(){
+		$(".menulateral").toggleClass("ocultaMenu");
+	});
 }
