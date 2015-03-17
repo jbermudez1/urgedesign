d8fac65940929365a060655b944339e4eaea7d46
jbermudez1 <javierbermudez@MacBook-Pro-de-Javier-2.local>
2015-03-16 21:46:25 -0700

    Agregando filtros

--- js/urge.js
+++ js/urge.js
@@ -3,13 +3,28 @@ $(document).on( "ready", inicio );
 function inicio (){
 	
 	$( "nav" ).load( "menu.html", vermenu);
-	
-	$("#filtroDos").hide();
-
 	$("#footer").load("footer.html");
 
+	$(".filtro1").click(function(){
+		$(".buscador").fadeOut();
+		$("#filtroUno").fadeOut(function(){
+			$("#filtroDos").fadeIn();
+		});
+	})
 
-}
+	$("#filtroDos > select").change(function(){
+	
+		$("#filtroDos").fadeOut( function(){
+			$("#filtroTres").fadeIn();
+		});
+	})
+
+	$("#filtroTres > select").change(function(){
+		window.location.href = "resultadoTramites.html";
+	})
+
+
+	}
 
 function vermenu (){
 	$(".verMenu").click( function(){
